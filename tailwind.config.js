45c972c492e86f26e1e2c6f122ad9bc18cb645af
JIrakiat Changsalak <75299841+jirakiat@users.noreply.github.com>
2026-02-05 14:24:05 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        kanit: ['Kanit', 'sans-serif'],
+        sans: ['Kanit', 'sans-serif'],
       },
+      animation: {
+        shine: 'shine 2s infinite linear',
+      },
+      keyframes: {
+        shine: {
+          '0%': { left: '-100%', opacity: '0' },
+          '50%': { opacity: '0.5' },
+          '100%': { left: '200%', opacity: '0' },
+        }
+      }
     },
   },
   plugins: [],
